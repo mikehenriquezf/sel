8ab7366cbeba277a326f0e0cbc00fba4beec625c
Mike Henriquez <mike.henriquezf@gmail.com>
2013-04-05 13:02:32 -0400

    Text added in qr page

--- qr/index.php
+++ qr/index.php
@@ -35,6 +35,95 @@
 		<p>Proyecto que logra unir micro proyectos de la escena de arte independiente durante un mismo fin de semana en Puerto Rico.</p> 
 		<p>Santurce es Ley es el primer ftestival cultural organizado por artistas, galerístas independientes y la comunidad, con el propósito de activar un circuito de arte en Santurce.</p> 
 		<p>Esta edición se celebrará el fin de semana del viernes, 5 al domingo, 7 de abirl de 2013.</p>
+		<h3>Ruta del arte:</h3>
+ 
+<p>·      Viernes 5:30 - 12media noche</p>
+
+<p>·      Trolley de 5pm- 10pm</p>
+
+<p>·      Sábado 12md-12medianoche</p>
+
+<p>·      Trolley 12md - 5pm</p> 
+
+ 
+<p>TARIMA PRINCIPAL (Calle Cerra 734, curado por hilife.bandcamp.com):</p>
+ 
+<h3>VIERNES, 5 DE ABRIL</h3>
+ 
+<p>4:00pm – 5:00pm: Lossless</p>
+ 
+<p>5:00pm – 6:00pm: Lobogamma
+www.lobogamma.com</p>
+ 
+<p>6:00pm – 7:00pm: Waz
+https://www.facebook.com/waz.boyellucievantolico</p>
+ 
+<p>7:00pm – 8:00pm: Discuss
+https://www.facebook.com/iamdiscuss</p>
+ 
+<p>8:00pm – 9:00pm: MoreIra
+https://www.facebook.com/moreiramusica</p>
+ 
+<p>9:00pm – 10:00pm: Dax Díaz
+https://www.facebook.com/pages/Dax-Diaz/211609468978571</p>
+ 
+<p>10:00pm – 11:00pm: Unochosiete
+http://unochosiete.bandcamp.com/</p>
+ 
+<p>11:00pm – 12:00pm:Eduardo Alegría
+https://www.facebook.com/alegriarampante</p>
+ 
+<p>*12:00pm: After Party @ La Respuesta: Fofe y los Fetiches</p>
+ 
+<h3>SÁBADO, 6 DE ABRIL</h3>
+ 
+<p>2:00pm – 3:00pm: Terminal 1981</p>
+ 
+<p>3:00pm – 4:00pm: Scdd/Alta
+https://www.facebook.com/pages/ScddAlta/411058125609038</p>
+ 
+<p>4:00pm – 5:00pm: DJ Wakatela
+www.calle13.com</p>
+ 
+<p>5:00pm – 6:00pm: Messy Deprát
+https://soundcloud.com/messydeprat</p>
+ 
+<p>6:00pm – 7:00pm: Overlord
+https://www.facebook.com/overlordmusic</p>
+ 
+<p>7:00pm – 8:00pm: Carabalí</p>
+ 
+<p>8:00pm – 9:00pm: Campo-Formio
+https://www.facebook.com/CampoFormio</p>
+ 
+<p>9:00pm – 10:00pm: Jazz Bandana
+https://www.facebook.com/jbandana</p>
+ 
+<p>10:00pm – 11:00pm: Blu Bamboo Ensemble
+https://www.facebook.com/BluBamboo</p>
+ 
+<p>11:00pm – 12:00pm Dj Nature
+https://soundcloud.com/mark-o-lino</p>
+ 
+<p>*12:00pm After Party Oficial SEL4 @ La Respuesta
+Füete Billēte / Alvaro Díaz / El Duque de Santurce / Overlord
+https://www.facebook.com/events/430046117088814</p>
+ 
+<h3>DOMINGO, 8 DE ABRIL</h3>
+ 
+<p>Día familiar en el MAPR:</p>
+ 
+<p>1:00pm-2:00pm: Espectáculo para niños por María Juseppe</p>
+ 
+<p>2:00pm-3:00pm: Recorrido guiado Interconexiones</p>
+ 
+<p>3:00pm-4:00pm: Función para niños por Poncili</p>
+ 
+<p>5:00pm-7:00pm: Presentación cortometrajes NEO-DIVEDCO y conversatorio con artistas.</p>
+ 
+<p>Entrada libre de costo. - http://www.mapr.org</p>
+ 
+
 
 		<h3>Enlaces:</h3>
 
